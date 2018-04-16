d9d3c2ca9b128aefb84cbf6268ee5b3c35695c59
崇志 <chongzhi.ldq@taobao.com>
2018-04-16 20:47:14 +0800

    dev

--- build.js
+++ build.js
@@ -1,4 +1,13 @@
+/**
+ * master分支存的是文档 markdown 源码
+ * gh-pages存的是编译后的文档，对接github pages
+ * 执行 node build 会自动提交master代码并切换到gh-pages分支，删除旧文件，从_book里复制新编译的文件到根目录下
+ */
+
 const { execSync, spawnSync } = require('child_process')
+const path = require('path')
+const fs = require('fs')
+const fse = require('fs-extra')
 const config = {
     stdio: 'inherit'
 }
@@ -9,3 +18,17 @@ spawnSync('git', ['commit', '-m', '"modify book"'], config)
 spawnSync('git', ['pull', 'origin', 'master'], config)
 spawnSync('git', ['push', 'origin', 'master'], config)
 spawnSync('git', ['checkout', 'gh-pages'], config)
+
+
+let files = fs.readdirSync(process.cwd())
+const notDeleteFiles = ['.git', '_book', 'test.js', 'node_modules']
+for (const file of files) {
+    if (notDeleteFiles.indexOf(file) === -1)  {
+        spawnSync('rm', ['-rf', file])
+    }
+}
+
+let copyFiles = fs.readdirSync(path.resolve(process.cwd(), './_book'))
+for (const file of copyFiles) {
+    fse.copySync(`${process.cwd()}/_book/${file}`, `${process.cwd()}/${file}`)
+}
