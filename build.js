dfd46ae92a9f590e70e1fc66f9f797b5a9df51a3
崇志 <chongzhi.ldq@taobao.com>
2018-04-16 17:41:05 +0800

    modify book

--- build.js
+++ build.js
@@ -1,2 +1,8 @@
 const execSync = require('child_process').execSync
+
+execSync('gitbook build')
+execSync('git add -A')
+execSync('git commit -m "modify book"')
+execSync('git pull origin master')
+execSync('git push origin master')
 execSync('git checkout gh-pages')
