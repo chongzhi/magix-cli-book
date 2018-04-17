3e50fc87c5b7a9a9d6f131c5e0b967648d3bdd37
崇志 <chongzhi.ldq@taobao.com>
2018-04-17 10:15:19 +0800

    "modify book"

--- publish.js
+++ publish.js
@@ -51,7 +51,7 @@ function execCommandReturn(command) {
     spawnSync('git', ['commit', '-m', '"modify book"'], config)
     spawnSync('git', ['pull', 'origin', 'master'], config)
     spawnSync('git', ['push', 'origin', 'master'], config)
-    spawnSync('git', ['checkout', 'gh-pages'], config)
+    spawnSync('git', ['checkout', '-B', 'gh-pages'], config)
 
     // let files = fs.readdirSync(process.cwd())
     // const notDeleteFiles = ['.git', '_book', 'node_modules']
